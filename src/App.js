3f69f03927040b4058dd1b2396e2c31087c46f0d
ooyendyk <ooyendyk@github.io>
2023-05-20 22:10:38 +0930

    Make YOUR_API_KEY a .env file, fix scoping issue

--- src/App.js
+++ src/App.js
@@ -2,61 +2,61 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const WeatherApp = () => {
-  const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState('');
+    const [weather, setWeather] = useState(null);
+    const [city, setCity] = useState('');
 
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      if (city) {
-        try {
-          const response = await axios.get(
-              `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`
-          );
-          setWeather(response.data);
-        } catch (error) {
-          console.error('Error fetching weather data:', error);
-          setWeather(null);
-        }
-      }
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            if (city) {
+                try {
+                    const response = await axios.get(
+                        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
+                    );
+                    setWeather(response.data);
+                } catch (error) {
+                    console.error('Error fetching weather data:', error);
+                    setWeather(null);
+                }
+            }
+        };
+
+        fetchWeatherData().then();
+    }, [city]);
+
+    const handleCityChange = (event) => {
+        setCity(event.target.value);
     };
 
-    fetchWeatherData().then();
-  }, [city]);
+    const renderWeatherInfo = () => {
+        if (!weather) {
+            return <p>No weather data available.</p>;
+        }
 
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
-  };
+        const { name, main, weather: weatherInfo } = weather;
 
-  const kelvinToCelsius = (temperature) => Math.round(temperature - 274.15);
+        const kelvinToCelsius = (temperature) => Math.round(temperature - 274.15);
 
-  const renderWeatherInfo = () => {
-    if (!weather) {
-      return <p>No weather data available.</p>;
-    }
-
-    const { name, main, weather: weatherInfo } = weather;
+        return (
+            <div>
+                <h2>{name}</h2>
+                <p>Temperature: {kelvinToCelsius(main.temp)}°C</p>
+                <p>Description: {weatherInfo[0].description}</p>
+            </div>
+        );
+    };
 
     return (
         <div>
-          <h2>{name}</h2>
-          <p>Temperature: {kelvinToCelsius(main.temp)}°C</p>
-          <p>Description: {weatherInfo[0].description}</p>
+            <h1>Weather App</h1>
+            <input
+                type="text"
+                value={city}
+                onChange={handleCityChange}
+                placeholder="Enter city"
+            />
+            {renderWeatherInfo()}
         </div>
     );
-  };
-
-  return (
-      <div>
-        <h1>Weather App</h1>
-        <input
-            type="text"
-            value={city}
-            onChange={handleCityChange}
-            placeholder="Enter city"
-        />
-        {renderWeatherInfo()}
-      </div>
-  );
 };
 
 export default WeatherApp;
