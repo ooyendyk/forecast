bd6587db36f792b333e097408fc160c889015b84
ooyendyk <ooyendyk@github.io>
2023-05-21 13:44:11 +0930

    Update to have daily and weekly forecast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const CurrentWeather = ({ weather }) => {
+    if (!weather) {
+        return <p>No weather data available.</p>;
+    }
+
+    const { city, list } = weather;
+    const kelvinToCelsius = (temperature) => Math.round(temperature - 273.15);
+    const currentForecast = list[0];
+
+    return (
+        <div>
+            <h2>{city.name}</h2>
+            <h3>Current Weather</h3>
+            <p>Date: {new Date(currentForecast.dt_txt).toLocaleDateString()}</p>
+            <p>Temperature: {kelvinToCelsius(currentForecast.main.temp)}°C</p>
+            <p>Description: {currentForecast.weather[0].description}</p>
+        </div>
+    );
+};
+
+const WeeklyForecast = ({ weather }) => {
+    if (!weather) {
+        return <p>No weather data available.</p>;
+    }
+
+    const { city, list } = weather;
+    const kelvinToCelsius = (temperature) => Math.round(temperature - 273.15);
+    const weeklyForecast = list.slice(1, 8); // Exclude the current forecast
+
+    return (
+        <div>
+            <h3>Weekly Forecast</h3>
+            {weeklyForecast.map((forecast) => (
+                <div key={forecast.dt}>
+                    <p>Date: {new Date(forecast.dt_txt).toLocaleDateString()}</p>
+                    <p>Temperature: {kelvinToCelsius(forecast.main.temp)}°C</p>
+                    <p>Description: {forecast.weather[0].description}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
 const WeatherApp = () => {
     const [weather, setWeather] = useState(null);
     const [city, setCity] = useState('');
@@ -27,31 +70,6 @@ const WeatherApp = () => {
         setCity(event.target.value);
     };
 
-    const renderWeatherInfo = () => {
-        if (!weather) {
-            return <p>No weather data available.</p>;
-        }
-
-        const { city, list } = weather;
-
-        const kelvinToCelsius = (temperature) => Math.round(temperature - 273.15);
-
-        const weeklyForecast = list.slice(0, 7); // Get the first 7 forecast entries for the week
-
-        return (
-            <div>
-                <h2>{city.name}</h2>
-                {weeklyForecast.map((forecast) => (
-                    <div key={forecast.dt}>
-                        <p>Date: {new Date(forecast.dt_txt).toLocaleDateString()}</p>
-                        <p>Temperature: {kelvinToCelsius(forecast.main.temp)}°C</p>
-                        <p>Description: {forecast.weather[0].description}</p>
-                    </div>
-                ))}
-            </div>
-        );
-    };
-
     return (
         <div>
             <h1>Weather App</h1>
@@ -61,7 +79,8 @@ const WeatherApp = () => {
                 onChange={handleCityChange}
                 placeholder="Enter city"
             />
-            {renderWeatherInfo()}
+            <CurrentWeather weather={weather} />
+            <WeeklyForecast weather={weather} />
         </div>
     );
 };
